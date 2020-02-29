0c41a667a488dec0e2b9b9a505ab10f3cb3dadbd
David Obodo <obododavid5@gmail.com>
2020-02-29 07:29:05 +0100

    using intersection observer

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
 //-----------------------------------------------------------------------------------
 //Method 1 (Vanilla JS)
 //-----------------------------------------------------------------------------------
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
+// document.addEventListener('DOMContentLoaded', handleLazyLoad);
+// function handleLazyLoad() {
+//     let lazyImages: any = document.querySelectorAll("img"); // ran from typescript
+//     let active = false;
+//     const lazyLoad = () => {
+//         if (active === false) {
+//             active = true;
+//             setTimeout(() => {
+//                 lazyImages.forEach(lazyImage => {
+//                     if (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) {
+//                         lazyImage.parentElement.classList.remove("loading");
+//                         lazyImage.src = lazyImage.dataset.src;
+//                         //remove the img already displaying from initial NodeList
+//                         lazyImages = [...lazyImages].filter(image => {
+//                             return image !== lazyImage;
+//                         });
+//                         console.log(lazyImages.length)
+//                         if (lazyImages.length === 0) {
+//                             document.removeEventListener("scroll", lazyLoad);
+//                             window.removeEventListener("resize", lazyLoad);
+//                             window.removeEventListener("orientationchange", lazyLoad);
+//                         }
+//                     }
+//                 })
+//                 active = false;
+//             }, 200);
+//         }
+//     };
+//     document.addEventListener("scroll", lazyLoad);
+//     window.addEventListener("resize", lazyLoad);
+//     window.addEventListener("orientationchange", lazyLoad);
+// }
+//-----------------------------------------------------------------------------------
+//Method 2 (Intersection Observer)
+//-----------------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', handleLazyLoad);
-// document.addEventListener('click', handleLazyLoad);
 function handleLazyLoad() {
-    var lazyImages = document.querySelectorAll("img"); // ran from typescript
-    var active = false;
-    var lazyLoad = function () {
-        if (active === false) {
-            active = true;
-            setTimeout(function () {
-                lazyImages.forEach(function (lazyImage) {
-                    if (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) {
-                        lazyImage.parentElement.classList.remove("loading");
-                        lazyImage.src = lazyImage.dataset.src;
-                        //remove the img already displaying from initial NodeList
-                        lazyImages = __spreadArrays(lazyImages).filter(function (image) {
-                            return image !== lazyImage;
-                        });
-                        console.log(lazyImages.length);
-                        if (lazyImages.length === 0) {
-                            document.removeEventListener("scroll", lazyLoad);
-                            window.removeEventListener("resize", lazyLoad);
-                            window.removeEventListener("orientationchange", lazyLoad);
-                        }
-                    }
-                });
-                active = false;
-            }, 200);
-        }
+    var lazyImages = document.querySelectorAll("img");
+    var options = {
+        rootOptions: '0px 0px 10px 0px'
     };
-    document.addEventListener("scroll", lazyLoad);
-    window.addEventListener("resize", lazyLoad);
-    window.addEventListener("orientationchange", lazyLoad);
+    var lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                var lazyImage = entry.target;
+                lazyImage.parentElement.classList.remove("loading");
+                lazyImage.src = lazyImage.dataset.src;
+                observer.unobserve(lazyImage);
+            }
+        });
+    }, options);
+    lazyImages.forEach(function (image) {
+        lazyImageObserver.observe(image);
+    });
 }
--- main.ts
+++ main.ts
@@ -2,44 +2,72 @@
 //Method 1 (Vanilla JS)
 //-----------------------------------------------------------------------------------
 
+// document.addEventListener('DOMContentLoaded', handleLazyLoad);
+
+// function handleLazyLoad() {
+//     let lazyImages: any = document.querySelectorAll("img"); // ran from typescript
+//     let active = false;
+
+//     const lazyLoad = () => {
+//         if (active === false) {
+//             active = true;
+
+//             setTimeout(() => {
+//                 lazyImages.forEach(lazyImage => {
+//                     if (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) {
+
+//                         lazyImage.parentElement.classList.remove("loading");
+//                         lazyImage.src = lazyImage.dataset.src;
+
+//                         //remove the img already displaying from initial NodeList
+//                         lazyImages = [...lazyImages].filter(image => {
+//                             return image !== lazyImage;
+//                         });
+
+//                         console.log(lazyImages.length)
+
+//                         if (lazyImages.length === 0) {
+//                             document.removeEventListener("scroll", lazyLoad);
+//                             window.removeEventListener("resize", lazyLoad);
+//                             window.removeEventListener("orientationchange", lazyLoad);
+//                         }
+//                     }
+//                 })
+//                 active = false;
+//             }, 200);
+//         }
+//     };
+
+//     document.addEventListener("scroll", lazyLoad);
+//     window.addEventListener("resize", lazyLoad);
+//     window.addEventListener("orientationchange", lazyLoad);
+// }
+
+//-----------------------------------------------------------------------------------
+//Method 2 (Intersection Observer)
+//-----------------------------------------------------------------------------------
+
 document.addEventListener('DOMContentLoaded', handleLazyLoad);
-// document.addEventListener('click', handleLazyLoad);
 
 function handleLazyLoad() {
-    let lazyImages: any = document.querySelectorAll("img"); // ran from typescript
-    let active = false;
+    let lazyImages: any = document.querySelectorAll("img");
 
-    const lazyLoad = () => {
-        if (active === false) {
-            active = true;
+    const options: any = {
+        rootOptions: '0px 0px 10px 0px'
+    }
 
-            setTimeout(() => {
-                lazyImages.forEach(lazyImage => {
-                    if (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) {
+    let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let lazyImage: any = entry.target;
+                lazyImage.parentElement.classList.remove("loading");
+                lazyImage.src = lazyImage.dataset.src;
+                observer.unobserve(lazyImage);
+            }
+        });
+    }, options);
 
-                        lazyImage.parentElement.classList.remove("loading");
-                        lazyImage.src = lazyImage.dataset.src;
-
-                        //remove the img already displaying from initial NodeList
-                        lazyImages = [...lazyImages].filter(image => {
-                            return image !== lazyImage;
-                        });
-
-                        console.log(lazyImages.length)
-
-                        if (lazyImages.length === 0) {
-                            document.removeEventListener("scroll", lazyLoad);
-                            window.removeEventListener("resize", lazyLoad);
-                            window.removeEventListener("orientationchange", lazyLoad);
-                        }
-                    }
-                })
-                active = false;
-            }, 200);
-        }
-    };
-
-    document.addEventListener("scroll", lazyLoad);
-    window.addEventListener("resize", lazyLoad);
-    window.addEventListener("orientationchange", lazyLoad);
+    lazyImages.forEach(image => {
+        lazyImageObserver.observe(image)
+    })
 }
